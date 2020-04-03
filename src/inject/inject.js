0d7ccded0b52f1f27ba49bbf1699495e3b8b1789
Niko Draca <me@nikodraca.com>
2020-04-03 09:08:05 -0400

    FIx broken listener

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,16 @@
 let CURRENT_SONG;
 
 const startListening = () => {
+  // get initial visualizer
+  fetchCurrentlyPlaying();
+
   const player = document.querySelector('.now-playing-bar__left');
 
   var config = { attributes: true, childList: true, subtree: true };
 
   const callback = (mutationsList, observer) => {
     for (let mutation of mutationsList) {
-      if (mutation.type === 'attributes' && mutation.target.className === 'cover-art-image cover-art-image-loaded') {
+      if (mutation.type === 'attributes' && mutation.target.className === 'cover-art-image') {
         fetchCurrentlyPlaying();
       }
     }
@@ -21,49 +24,54 @@ const startListening = () => {
 };
 
 const fetchCurrentlyPlaying = () => {
+  const nowPlaying = $('.now-playing').first();
+  const textSpans = nowPlaying.find('span[draggable="true"]').toArray();
+  const imageUrlStyle = nowPlaying
+    .find('.cover-art-image')
+    .first()
+    .css('background-image');
+
+  // TODO: Error handle
+  if (textSpans.length < 1) return;
+
+  const title = textSpans.shift().textContent;
+  const artist = textSpans.map(a => a.textContent).join(', ');
+  const imageUrl = /\("([^)]+)"\)/i.exec(imageUrlStyle)[1];
+  const id = `${title}:${artist}`;
+
   const newSong = {
-    title: $('.track-info__name')
-      .first()
-      .text(),
-    artist: $('.track-info__artists')
-      .first()
-      .text(),
-    image_url: $('.now-playing__cover-art .cover-art .cover-art-image')
-      .css('background-image')
-      .slice(5, -2),
-    href: $('.track-info__name .react-contextmenu-wrapper span a').attr('href')
+    title,
+    artist,
+    imageUrl,
+    id
   };
 
   // check to see if different song
-  if (!CURRENT_SONG || CURRENT_SONG.href !== newSong.href) {
+  if (!CURRENT_SONG || CURRENT_SONG.id !== newSong.id) {
     CURRENT_SONG = newSong;
-
     console.log(CURRENT_SONG);
-
     updateVisualizer();
   }
 };
 
 const updateVisualizer = () => {
+  const { imageUrl, title, artist } = CURRENT_SONG;
+
   // render html for visualizer
   $('#visualizer').html(`
+    <button id="close-btn">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+    </button>
 
-	<button id="close-btn">
-		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-	</button>
+    <img src="${imageUrl}" class="visualizer-img"></img>
 
-	<img src="${CURRENT_SONG.image_url}" class="visualizer-img"></img>
-
-	<img src="${CURRENT_SONG.image_url}" class="song-img"></img>
-	<h1 class="visualizer-text">${CURRENT_SONG.title}</h1>
-	<h3 class="visualizer-text">${CURRENT_SONG.artist}</h3>
+    <img src="${imageUrl}" class="song-img"></img>
+    <h1 class="visualizer-text">${title}</h1>
+    <h3 class="visualizer-text">${artist}</h3>
   `);
 
   // add event listeners for close
   $('#close-btn').click(closeVisualizer);
-
-  // inactive cursor logic
-  inactiveMouseCursor();
 };
 
 const closeVisualizer = e => {
@@ -76,6 +84,9 @@ const openVisualizer = e => {
 
   // append play controls
   $('.now-playing-bar__center').addClass('visualizer-player');
+
+  // inactive cursor logic
+  inactiveMouseCursor();
 };
 
 const inactiveMouseCursor = () => {
@@ -114,23 +125,23 @@ const inactiveMouseCursor = () => {
 
 chrome.extension.sendMessage({}, response => {
   const readyStateCheckInterval = setInterval(() => {
-    if (document.readyState === 'complete') {
+    if (document.readyState === 'complete' && $('.now-playing').length) {
       clearInterval(readyStateCheckInterval);
-
-      // start mutation observer for currently playing
-      startListening();
 
       // append visualizer
       $('body').append(`<div id="visualizer"></div>`);
 
+      // start mutation observer for currently playing
+      startListening();
+
       // add icon to controls
       $('.ExtraControls').prepend(`
-		<div class="GlueDropTarget">
-		<button id="show-visualizer">
-			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize-2"><polyline points="15 3 21 3 21 9"></polyline><polyline points="9 21 3 21 3 15"></polyline><line x1="21" y1="3" x2="14" y2="10"></line><line x1="3" y1="21" x2="10" y2="14"></line></svg>
-		</button>
-		</div>
-	  `);
+          <div class="GlueDropTarget">
+          <button id="show-visualizer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize-2"><polyline points="15 3 21 3 21 9"></polyline><polyline points="9 21 3 21 3 15"></polyline><line x1="21" y1="3" x2="14" y2="10"></line><line x1="3" y1="21" x2="10" y2="14"></line></svg>
+          </button>
+          </div>
+      `);
 
       // add event listener to open
       $('#show-visualizer').click(openVisualizer);
