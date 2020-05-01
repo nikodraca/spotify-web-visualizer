7105ededd8069aa051b1498076f2db8841dcb1ac
Niko Draca <me@nikodraca.com>
2020-04-30 21:49:03 -0400

    Fix listener + lazy load images

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,7 +10,7 @@ const startListening = () => {
 
   const callback = (mutationsList, observer) => {
     for (let mutation of mutationsList) {
-      if (mutation.type === 'attributes' && mutation.target.className === 'cover-art-image') {
+      if (mutation.type === 'attributes' && mutation.target.nodeName === 'A') {
         fetchCurrentlyPlaying();
       }
     }
@@ -26,30 +26,25 @@ const startListening = () => {
 const fetchCurrentlyPlaying = () => {
   const nowPlaying = $('.now-playing').first();
   const textSpans = nowPlaying.find('span[draggable="true"]').toArray();
-  const imageUrlStyle = nowPlaying
-    .find('.cover-art-image')
-    .first()
-    .css('background-image');
+  const imageUrl = nowPlaying.find('.cover-art-image').first().attr('src');
 
   // TODO: Error handle
   if (textSpans.length < 1) return;
 
   const title = textSpans.shift().textContent;
-  const artist = textSpans.map(a => a.textContent).join(', ');
-  const imageUrl = /\("([^)]+)"\)/i.exec(imageUrlStyle)[1];
+  const artist = textSpans.map((a) => a.textContent).join(', ');
   const id = `${title}:${artist}`;
 
   const newSong = {
     title,
     artist,
     imageUrl,
-    id
+    id,
   };
 
   // check to see if different song
   if (!CURRENT_SONG || CURRENT_SONG.id !== newSong.id) {
     CURRENT_SONG = newSong;
-    console.log(CURRENT_SONG);
     updateVisualizer();
   }
 };
@@ -57,36 +52,43 @@ const fetchCurrentlyPlaying = () => {
 const updateVisualizer = () => {
   const { imageUrl, title, artist } = CURRENT_SONG;
 
-  // render html for visualizer
-  $('#visualizer').html(`
+  // preload image so the visualizer doesn't lag
+  const preloadedImage = new Image();
+  preloadedImage.src = imageUrl;
+
+  preloadedImage.onload = function () {
+    // render html for visualizer
+    $('#visualizer').html(`
     <button id="close-btn">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
     </button>
 
     <img src="${imageUrl}" class="visualizer-img"></img>
 
-    <img src="${imageUrl}" class="song-img"></img>
     <h1 class="visualizer-text">${title}</h1>
     <h3 class="visualizer-text">${artist}</h3>
   `);
 
-  // add event listeners for close
-  $('#close-btn').click(closeVisualizer);
+    // add event listeners for close
+    $('#close-btn').click(closeVisualizer);
+  };
 };
 
-const closeVisualizer = e => {
+const closeVisualizer = (e) => {
   $('#visualizer').css('display', 'none');
   $('.now-playing-bar__center').removeClass('visualizer-player');
 };
 
-const openVisualizer = e => {
-  $('#visualizer').css('display', 'flex');
+const openVisualizer = (e) => {
+  if (CURRENT_SONG) {
+    $('#visualizer').css('display', 'flex');
 
-  // append play controls
-  $('.now-playing-bar__center').addClass('visualizer-player');
+    // append play controls
+    $('.now-playing-bar__center').addClass('visualizer-player');
 
-  // inactive cursor logic
-  inactiveMouseCursor();
+    // inactive cursor logic
+    inactiveMouseCursor();
+  }
 };
 
 const inactiveMouseCursor = () => {
@@ -101,29 +103,29 @@ const inactiveMouseCursor = () => {
       }
 
       $('html').css({
-        cursor: ''
+        cursor: '',
       });
     } else {
       $('#visualizer').css({
-        cursor: 'default'
+        cursor: 'default',
       });
       fadeInBuffer = false;
     }
 
     timer = setTimeout(() => {
       $('#visualizer').css({
-        cursor: 'none'
+        cursor: 'none',
       });
 
       fadeInBuffer = true;
     }, 2000);
   });
   $('#visualizer').css({
-    cursor: 'default'
+    cursor: 'default',
   });
 };
 
-chrome.extension.sendMessage({}, response => {
+chrome.extension.sendMessage({}, (response) => {
   const readyStateCheckInterval = setInterval(() => {
     if (document.readyState === 'complete' && $('.now-playing').length) {
       clearInterval(readyStateCheckInterval);
